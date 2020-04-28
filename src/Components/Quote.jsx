fd1e0150aaa38ceaf49225d77633ddec8bd97ef2
bdiniscia <bdiniscia@gmail.com>
2020-04-28 15:16:35 -0400

    Ready the logic and the styles

--- src/Components/Quote.jsx
+++ src/Components/Quote.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import './Quote.css'
 
 export default class Quote extends Component {
+
+    componentDidMount () {
+        console.log('>>Desde el Quote:', this.props.quotes)
+    }
+
+    renderQuote = () => {
+        return (
+            <div className='divTextQuote'> 
+                <h3 className='quoteText'>"{this.props.quotes[this.props.quoteNumber].text}"</h3>
+                <p className='quoteAuthor'>- {this.props.quotes[this.props.quoteNumber].author}</p>
+            </div>
+        )
+    }
+
+    
     render() {
         return (
-            <div>
-                
+            <div className='containerQuote'>
+                {this.props.quotes && this.renderQuote()}
             </div>
         )
     }
--- src/Components/Template.jsx
+++ src/Components/Template.jsx
@@ -4,17 +4,56 @@ import Quote from './Quote'
 import './Template.css'
 
 export default class Template extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            lengthArray: null,
+            quoteNumber: 0,
+            quotes: null,
+        }
+    }
+
+    randomNumOfTheArray = () => {
+        let randomNumer = Math.floor((Math.random() * this.state.lengthArray));
+        this.setState({
+            quoteNumber: randomNumer
+        })
+        console.log('El random number:', this.state.quoteNumber)
+    }
+
+    callingTheApi = () => {
+        fetch('https://type.fit/api/quotes')
+            .then( res => res.json())
+            .then( data => {
+                console.log(data[199].text);
+                this.setState({
+                    lengthArray: data.length,
+                    quotes: data,
+                })
+                console.log('Estado del lenght:', this.state.lengthArray)
+                console.log('Estado de las quotes:', this.state.quotes)
+            })
+            .catch( err => console.error(err))
+    }
+
+    componentDidMount () {
+        this.callingTheApi()
+    }
 
     render() {
         return (
             <div className='container'>
                 <img className='logo' alt='Logo of Inspire Me' src={require('../img/logo.png')} />
                 <h1 className='motto'>Get inspired with a quote and face the day with your best energy.</h1>
-                <p className='instructions'>Click the button to get a random quote</p>
+                <p className='instructions'>Click the button to get a random quote:</p>
                 <Button 
                     title='Click me!'
+                    onClick={() => this.randomNumOfTheArray()}
                 />
-                <Quote />        
+                <Quote 
+                    quoteNumber= {this.state.quoteNumber}
+                    quotes= {this.state.quotes}
+                />        
             </div>
         )
     }
